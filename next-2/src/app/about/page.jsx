8ee5a1dc417e548246ced1d019f25a621853ea69
mohammad-amin-nik-hedayati <amin.nik777@gmail.com>
2024-08-13 16:04:00 +0330

    changes

--- next-2/src/app/about/page.jsx
+++
@@ -1,9 +0,0 @@
-function about() {
-    return (
-        <>
-            <h1>this is about page</h1>
-        </>
-    );
-}
-
-export default about;
--- next-2/src/app/store/page.jsx
+++
@@ -1,9 +0,0 @@
-function store() {
-    return (
-        <>
-            <h1>this is store page</h1>
-        </>
-    );
-}
-
-export default store;
