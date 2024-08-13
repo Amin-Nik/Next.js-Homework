22ca40cc27fb891556e6df11d2953a2c46a18ea7
Amin Nik <amin.nik777@gmail.com>
2024-08-13 16:09:48 +0330

    Delete next-1 directory

--- next-1/src/app/store/shoes/page.jsx
+++
@@ -1,9 +0,0 @@
-function shoes() {
-    return (
-        <>
-            <h1>shoe page</h1>
-        </>
-    );
-}
-
-export default shoes;
--- next-1/src/app/support/page.jsx
+++
@@ -1,9 +0,0 @@
-function support() {
-    return (
-        <>
-            <h1>this is support page</h1>
-        </>
-    );
-}
-
-export default support;
