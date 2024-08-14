b918d5920513d0d60d423349a83f05edcf0c17ba
mohammad-amin-nik-hedayati <amin.nik777@gmail.com>
2024-08-14 18:32:41 +0330

    1 id sone

--- next-3/src/app/page.jsx
+++ next-3/src/app/page.jsx
@@ -1,9 +1,53 @@
-import Image from "next/image";
+import { fetchData } from '../utils/dataFetcher';
 
-export default function Home() {
+export default async function Home() {
+
+  const userData = await fetchData("https://dummyjson.com/users");
+  const postData = await fetchData("https://dummyjson.com/posts");
+  const recipeData = await fetchData("https://dummyjson.com/recipes");
+
   return (
     <>
       <h1>this is home page</h1>
+      <br /><br />
+      <section>
+        <h2>this is users first name:</h2>
+        <ul>
+          {
+            userData.users.map((data, index) => {
+              return <li key={index}>
+                {data.firstName}
+              </li>
+            })
+          }
+        </ul>
+      </section>
+      <hr /><hr />
+      <section>
+        <h2>this is posts title:</h2>
+        <ul>
+          {
+            postData.posts.map((data, index) => {
+              return <li key={index}>
+                {data.title}
+              </li>
+            })
+          }
+        </ul>
+      </section>
+      <hr /><hr />
+      <section>
+        <h2>this is recipes name:</h2>
+        <ul>
+          {
+            recipeData.recipes.map((data, index) => {
+              return <li key={index}>
+                {data.name}
+              </li>
+            })
+          }
+        </ul>
+      </section>
     </>
   );
 }
