d590fa227d05bd7b8a131b934bdf731ef343e720
mohammad-amin-nik-hedayati <amin.nik777@gmail.com>
2024-08-17 14:00:40 +0330

    2 is done

---
+++ next-3/src/app/loading.jsx
@@ -0,0 +1,17 @@
+function loading() {
+    return (
+        <div style={{
+            height: "90vh",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "white",
+            backgroundColor: "darkblue"
+        }}>
+            <h1>Loading... Please Wait</h1>
+        </div>
+    );
+}
+
+export default loading;
